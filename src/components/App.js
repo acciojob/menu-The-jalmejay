70c9e0f1b8b9d2512e809a30a27b537a2607c753
JALMEJAY KUMAR SHAW <116828650+The-jalmejay@users.noreply.github.com>
2025-10-04 21:22:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,18 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from "react";
 
+const App=()=>{
+return(
+  <div className="container">
+  <h1 className="heading">Our Menu</h1>
+  <div className="underLine"></div>
+  <ul className="menu-filter-list">
+  <li className="filter">All</li>
+  <li className="filter">BreakFast</li>
+  <li className="filter">Lunch</li>
+  <li className="filter">Shakes</li>
+  </ul>
+  
+  </div>
+);
+};
+export default App;
