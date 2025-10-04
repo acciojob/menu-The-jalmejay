55bfa210c6466db7cfb332c99e02bb9aeea7ef7b
JALMEJAY KUMAR SHAW <116828650+The-jalmejay@users.noreply.github.com>
2025-10-04 23:21:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -174,16 +174,16 @@ const App = () => {
       <h1 className="heading">Our Menu</h1>
       <div className="underLine"></div>
       <ul className="menu-filter-list">
-        <li className="filter" onClick={() => handleFilter("")}>
+        <li className="filter" id="filter-btn-1" onClick={() => handleFilter("")}>
           All
         </li>
-        <li className="filter" id="filter-btn-1" onClick={() => handleFilter("BreakFast")}>
+        <li className="filter" id="filter-btn-2" onClick={() => handleFilter("BreakFast")}>
           BreakFast
         </li>
-        <li className="filter" id="filter-btn-2" onClick={() => handleFilter("Lunch")}>
+        <li className="filter" id="filter-btn-3" onClick={() => handleFilter("Lunch")}>
           Lunch
         </li>
-        <li className="filter" id="filter-btn-3" onClick={() => handleFilter("Shakes")}>
+        <li className="filter" id="filter-btn-4" onClick={() => handleFilter("Shakes")}>
           Shakes
         </li>
       </ul>
