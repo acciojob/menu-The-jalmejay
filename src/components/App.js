a085450e9741dcd44b8f27e71c9f5ce62f0edba7
JALMEJAY KUMAR SHAW <116828650+The-jalmejay@users.noreply.github.com>
2025-10-04 23:30:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./styles.css";
-import data form "./data"
+import data from "./data"
 import Menu from "./Menu";
 const App = () => {
   const [fil, handleFilter] = useState("");
@@ -13,16 +13,16 @@ const App = () => {
       <h1 className="heading">Our Menu</h1>
       <div className="underLine"></div>
       <ul className="menu-filter-list">
-        <li className="filter" id="filter-btn-1" onClick={() => handleFilter("")}>
+        <li className="filter"  onClick={() => handleFilter("")}>
           All
         </li>
-        <li className="filter" id="filter-btn-2" onClick={() => handleFilter("BreakFast")}>
+        <li className="filter" id="filter-btn-1" onClick={() => handleFilter("BreakFast")}>
           BreakFast
         </li>
-        <li className="filter" id="filter-btn-3" onClick={() => handleFilter("Lunch")}>
+        <li className="filter" id="filter-btn-2" onClick={() => handleFilter("Lunch")}>
           Lunch
         </li>
-        <li className="filter" id="filter-btn-4" onClick={() => handleFilter("Shakes")}>
+        <li className="filter" id="filter-btn-3" onClick={() => handleFilter("Shakes")}>
           Shakes
         </li>
       </ul>
