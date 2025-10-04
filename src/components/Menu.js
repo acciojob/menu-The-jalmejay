0b43926f309d5bbfb59d27fea845cc426b055654
JALMEJAY KUMAR SHAW <116828650+The-jalmejay@users.noreply.github.com>
2025-10-04 22:53:05 +0530

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,27 @@
 import React from "react";
+import "./styles.css";
 
-const Menu=()=>{
-  return(
-    <div>
-    
+const Menu = ({ data }) => {
+  console.log(data);
+  return (
+    <div className="menus">
+      {data &&
+        data.map((e) => {
+          return (
+            <div key={e.id} className="menu-item">
+              <img src={e.img} alt={e.name} className="photo" />
+              <div className="item-info">
+                <header>
+                  <h4>{e.name}</h4>
+                  <h4 className="price">${e.price}</h4>
+                </header>
+                <p className="item-text">{e.desc}</p>
+              </div>
+            </div>
+          );
+        })}
     </div>
-  )
+  );
 };
 
 export default Menu;
